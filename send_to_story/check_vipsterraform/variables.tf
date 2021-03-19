5db9980b34746c1779309000161c0e3c773856a7
hinchy <eoin+github@tines.io>
2021-03-19 15:20:04 +0000

    update mimecast readme

---
+++ send_to_story/check_vipsterraform/variables.tf
@@ -0,0 +1,20 @@
+variable "tines_token" {
+    type = string
+}
+
+variable "tines_email" {
+    type = string
+}
+
+variable "tines_base_url" {
+    type = string
+}
+
+variable "story_id" {
+    type = number
+}
+
+variable "team_id" {
+    type = number
+    default = "1"
+}
